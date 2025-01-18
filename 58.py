f06cb86f7d7fcd6c39470d7f38ebfb30f30ae04a
Thejus Rajendran <thejusr1999@icloud.com>
2025-01-18 21:05:33 +0530

    length of the last word

---
+++ 58.py
@@ -0,0 +1,19 @@
+# Leetcode 58: Length of the Last Word
+
+def lengthOfLastWord(s: str) -> int:
+    res = s.split(" ")
+    print(res)
+    l = len(res)
+    if l == 1:
+        return len(res[0])
+    else:
+        while(l > 0):
+            if len(res[l-1]) != 0:
+                return len(res[l-1])
+            l -= 1
+
+    return 0
+
+#driver code
+res = lengthOfLastWord("a ")
+print(res)
