d01b43ad37a941671727171300a22c1d42516aa8
Thejus Rajendran <thejusr1999@icloud.com>
2025-03-24 18:33:34 +0530

    done: leetcode 29

--- problems/divide_two_integers/solution.py
+++ problems/divide_two_integers/solution.py
@@ -1,2 +1,26 @@
-def add(a, b):
-    return a + b
+def solution(dividend: int, divisor: int) -> int:
+    if dividend == divisor: return 1
+    sign = 1
+    if dividend < 0 and divisor > 0 or dividend > 0 and divisor < 0:
+        sign = -1
+
+    n = abs(dividend)
+    d = abs(divisor)
+    qoutient = 0
+    while(n >= d):
+        count = 0
+        while(n >= (d << (count + 1))):
+            count += 1
+        qoutient += 1 << count
+        n -= d << count
+
+    if (sign == 1 and qoutient >= 1<<31):
+        return (1<<31) - 1
+    
+    if (sign == -1 and qoutient >= 1<<31):
+        return sign * 1<<31
+
+    return sign * qoutient
+
+res = solution(-2147483648, -1)
+print(res, (1<<31)-1)
--- problems/divide_two_integers/test_solution.py
+++ problems/divide_two_integers/test_solution.py
@@ -1,9 +1,18 @@
 import unittest
-from solution import add
+from solution import solution
 
 class TestSolution(unittest.TestCase):
     def test_case1(self):
-        self.assertEqual(add(2,4), 6)
+        self.assertEqual(solution(10,3), 3)
+
+    def test_case2(self):
+        self.assertEqual(solution(7,-3), -2)
+
+    def test_case3(self):
+        self.assertEqual(solution(7,7), 1)
+
+    def test_case4(self):
+        self.assertEqual(solution(-2147483648,-1), 2147483647)
 
 if __name__ == '__main__':
     unittest.main()
